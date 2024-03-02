cd274088a405801ea1b10e71ce6f1c31a82a5a41
Vaibhav Vemula <vaibhav@Vaibhavs-MacBook-Pro.local>
2024-03-02 09:13:55 -0700

    Queu - Queue up with QUEU

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Maps from "./pages/Maps";
+import Community from "./pages/Community";
+import Profile from "./pages/Profile";
+import Tickets from "./pages/Tickets";
+import Likes from "./pages/Likes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/maps" element={<Maps />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/likes" element={<Likes />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
